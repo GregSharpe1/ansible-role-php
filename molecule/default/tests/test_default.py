8dd8801b7c10106c3ed78aa190f12228ded600af
Nathan Davies <nathan.davies@devopsguys.com>
2017-11-14 17:16:39 +0000

    Tests added and failing. Liniting errors corrected

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,27 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.fixture()
+def defaults(Ansible):
+    return Ansible("include_vars", "../../defaults/main.yml")["ansible_facts"]
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_php_fpm_install(host):
+    assert host.package("php-fpm").is_installed
+
+
+def test_php_extensions_install(host, defaults):
+    for package in defaults['php_extensions']:
+        assert host.package(package).is_installed
+
+
+def test_php_ini_security_fix(host):
+    php_ini = host.file("/etc/php/7.0/fpm/php.ini")
+
+    assert php_ini.contains("cgi.fix_pathinfo=0")
+    assert not php_ini.contains(";cgi.fix_pathinfo")
